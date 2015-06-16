17a70de7667ba6c95700a4dda89c2c97346e22cd
spajic <bestspajic@gmail.com>
2015-06-16 14:47:19 +0300

    remove Turtle mocks and Trivial classes left from Boilerplate

--- FileStorageTest.cpp
+++ FileStorageTest.cpp
@@ -22,24 +22,6 @@ using std::vector;
 
 const string kTempPath = "tests_temp\\";
 
-//class MockTurtle : public Turtle {
-//public:
-//	MOCK_METHOD0(PenUp, void());
-//	MOCK_METHOD0(PenDown, void());
-//	MOCK_METHOD1(Forward, void(int distance));
-//	MOCK_METHOD1(Turn, void(int degrees));
-//	MOCK_METHOD2(GoTo, void(int x, int y));
-//	MOCK_CONST_METHOD0(GetX, int());
-//	MOCK_CONST_METHOD0(GetY, int());
-//};
-//
-//TEST(PainterTest, CanDrawSomething) {
-//	MockTurtle Turtle;
-//	EXPECT_CALL(Turtle, PenDown())
-//		.Times(::testing::AtLeast(1));
-//	Turtle.PenDown();
-//}
-
 TEST(FileUtilsTest, CanReadFileToVectorOfChars) {
 	vector<char> generated_chars{'t', 'e', 's', 't', ' ', 'u', 't', 'i', 'l', 's' };
 	const string generated_file_path = kTempPath + "FileUtilsTest.bin";
