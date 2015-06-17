0f5639307784d4559f03108df44f8e2b4795a1be
Alexey Vasilyev <bestspajic@gmail.com>
2015-06-17 14:05:39 +0300

    add first code examples for Sqlite work

--- FileStorage/SqliteFileStorage.cpp
+++ FileStorage/SqliteFileStorage.cpp
@@ -8,7 +8,61 @@
 #include "sqlite3.h"
 
 SqliteFileStorage::SqliteFileStorage() {
+	InitDatabase("MyDb.db");
+}
 
+void SqliteFileStorage::InitDatabase(std::string file_name) {
+	db_name = file_name;
+	int rc;
+	char *error;
+	rc = sqlite3_open(db_name.c_str(), &db);
+	const char *sqlCreateTable = "CREATE TABLE MyTable (id INTEGER PRIMARY KEY, value STRING);";
+	rc = sqlite3_exec(db, sqlCreateTable, NULL, NULL, &error);
+	const char *sqlInsert = "INSERT INTO MyTable VALUES(NULL, 'A Value');";
+	rc = sqlite3_exec(db, sqlInsert, NULL, NULL, &error);
+	const char *sqlSelect = "SELECT * FROM MyTable;";
+	char **results = NULL;
+	int rows, columns;
+	sqlite3_get_table(db, sqlSelect, &results, &rows, &columns, &error);
+	
+	sqlite3_free_table(results);
+	//else
+	//{
+	//	// Display Table
+	//	for (int rowCtr = 0; rowCtr <= rows; ++rowCtr)
+	//	{
+	//		for (int colCtr = 0; colCtr < columns; ++colCtr)
+	//		{
+	//			// Determine Cell Position
+	//			int cellPosition = (rowCtr * columns) + colCtr;
+
+	//			// Display Cell Value
+	//			cout.width(12);
+	//			cout.setf(ios::left);
+	//			cout << results[cellPosition] << " ";
+	//		}
+
+	//		// End Line
+	//		cout << endl;
+
+	//		// Display Separator For Header
+	//		if (0 == rowCtr)
+	//		{
+	//			for (int colCtr = 0; colCtr < columns; ++colCtr)
+	//			{
+	//				cout.width(12);
+	//				cout.setf(ios::left);
+	//				cout << "~~~~~~~~~~~~ ";
+	//			}
+	//			cout << endl;
+	//		}
+	//	}
+	//}
+
+}
+
+SqliteFileStorage::~SqliteFileStorage() {
+	sqlite3_close(db);
 }
 
 void SqliteFileStorage::StoreFile(string name, string read_from) {
--- FileStorage/SqliteFileStorage.h
+++ FileStorage/SqliteFileStorage.h
@@ -5,13 +5,19 @@
 #include <map>
 #include <vector>
 
+#include "sqlite3.h"
+
 class SqliteFileStorage : public FileStorage {
 public:
 	SqliteFileStorage();
+	~SqliteFileStorage();
 	void StoreFile(string name, string read_from);
 	void RetreiveFile(string name, string store_to);
 	void RemoveFile(string name);
 	bool HasFile(string name);
 	std::vector<string> GetFileNamesList();
+	void InitDatabase(std::string file_name);
 private:
+	sqlite3 *db;
+	std::string db_name;
 };
